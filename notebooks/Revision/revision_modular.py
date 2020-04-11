926d80a81c278ab7c7821a1374df93c674af9f9a
Karla Alfaro Pizaña <alpika_191@hotmail.com>
2020-04-10 21:37:33 -0500

    insertar las funciones crea_matrices y factoriza_ply al script principal

--- notebooks/Revision/revision_modular.py
+++ notebooks/Revision/revision_modular.py
@@ -2,8 +2,18 @@ import numpy as np
 import pprint
 import pandas as pd
 #import factorizacion_PLU
-import crea_matrices
-import factoriza_plu
+
+def crea_matrices(dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup):
+    n=np.random.randint(dim_limite_inf, dim_limite_sup)
+    A=np.array(np.random.randint(entradas_lim_inf,entradas_lim_sup, size=(n, n)))
+    return A,n
+
+def factoriza_plu(A):
+    start_time=time.time()
+    P,L,U=factorizacion_PLU.PLU(A)
+    end_time=time.time()
+    tiempo_total = end_time-start_time
+    return tiempo_total, P, L, U
 
 def revision_PLU(nombre_archivo,num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup):
     f= open(nombre_archivo,"w")
@@ -13,11 +23,11 @@ def revision_PLU(nombre_archivo,num_corridas,dim_limite_inf,dim_limite_sup,entra
     #tipo_matriz=[]
     for i in range(0,num_corridas):
         
-        #modulo que crea la matriz random
+        #modulo que crea matrices de forma aleatoria
         A,n=crea_matrices.crea_matrices(dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup)
         dimension.append(n)
         
-        #modulo de conteo de tiempo
+        #modulo que implementa el algoritmo PLU y cuenta el tiempo
         tiempo_total, P, L, U=factoriza_plu.factoriza_plu(A)
         tiempo_plu.append(tiempo_total)
         
