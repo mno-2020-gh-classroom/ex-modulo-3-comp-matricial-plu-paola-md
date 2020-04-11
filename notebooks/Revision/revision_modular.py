df62a903f50f24964157dbe4acc00fedd578d22f
Karla Alfaro Pizaña <alpika_191@hotmail.com>
2020-04-10 23:59:30 -0500

    agregar documentacion crea_matrices y factoriza_plu

--- notebooks/Revision/revision_modular.py
+++ notebooks/Revision/revision_modular.py
@@ -5,12 +5,73 @@ import time
 import factorizacion_PLU
 
 def crea_matrices(dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup):
+    '''
+    Esta función crea matrices de forma aleatoria. Se le debe especificar el límite inferior y        
+    superior de la dimensión de la matriz y el límite inferior y superior de los números de entrada a 
+    la matriz. La función devuelve una matriz A cuadrada de dimensión nxn:
+
+    ==========
+    * Entradas:
+        - dim_limite_inf (integer): número entero que corresponde a la dimensión mínima de la matriz
+        - dim_liminte_sup (integer): número entero que corresponde a la dimensión máxima de la matriz
+        - entradas_lim_inf (integer): número entero que corresponde al número más chico que puede tener la 
+        matriz como entrada
+        - entradas_lim_sup (integer): número entero que corresponde al número más grande que puede tener
+        la matriz como entrada
+    * Salidas:
+        - A (matriz): matriz cuadrada de dimensión nxn
+    ==========
+    Ejemplo:
+        >> dim_limite_inf = 2 
+        >> dim_liminte_sup = 10^4 
+        >> entradas_lim_inf = -99
+        >> entradas_lim_sup = 99 
+        >> crea_matrices(dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup)
+        > A
+        >array([[-28, -50,  79],
+                 [-22,  87, -23],
+                 [  0, -97,  22]])
+    '''
     n=np.random.randint(dim_limite_inf, dim_limite_sup)
     A=np.array(np.random.randint(entradas_lim_inf,entradas_lim_sup, size=(n, n)))
     x_real=np.array(np.random.randint(entradas_lim_inf,entradas_lim_sup, size=(n, 1)))
     return A,n,x_real
 
 def factoriza_plu(A):
+        '''
+    Esta función ejecuta el algoritmo de PLU y mide el tiempo de ejecución total del algoritmo. Se le debe 
+    especificar una matriz cuadrada A. La función devuelve el tiempo total en segundos de ejecución del 
+    algoritmo, 2 matrices (L,U) y un vector P.
+    
+    * Para ver más información sobre la función factorización_PLU, favor de consultar su documentación.
+
+    ==========
+    * Entradas:
+        - A: matriz cuadrada de dimensión nxn
+    * Salidas:
+        - P (vector): nx1, con los índices de las columnas intercambiadas en el pivoteo.
+        - L (matriz): matriz triangular inferior de nxn
+        - U (matriz): matriz triangular superior nxn
+        - timempo_total (float): tiempo total en segundos que tarda la ejecución del algoritmo PLU.
+    ==========
+    Ejemplo:
+        >>A = np.array([[2, 2, 3], [-4, -4, -3], [4, 8, 3]])
+        >>factoriza_plu(A)
+        >P
+        >array([[0., 1., 0.],
+               [0., 0., 1.],
+               [1., 0., 0.]])
+        >L
+        >array([[ 1. ,  0. ,  0. ],
+               [-1. ,  1. ,  0. ],
+               [-0.5,  0. ,  1. ]])
+        >U
+        >array([[-4. , -4. , -3. ],
+               [ 0. ,  4. ,  0. ],
+               [ 0. ,  0. ,  1.5]])
+        >tiempo_total
+        0.00083283
+        '''
     start_time=time.time()
     P,L,U=factorizacion_PLU.PLU(A)
     end_time=time.time()
