22721b923840d5afe4986d7a29ce79bbed55911f
alpika19186 <51685571+alpika19186@users.noreply.github.com>
2020-04-10 21:49:07 -0500

    borrar factoriza_plu.py, se agregó a script principal

--- notebooks/Revision/factoriza_plu.py
+++
@@ -1,9 +0,0 @@
-import time
-import factorizacion_PLU
-
-def factoriza_plu(A):
-    start_time=time.time()
-    P,L,U=factorizacion_PLU.PLU(A)
-    end_time=time.time()
-    tiempo_total = end_time-start_time
-    return tiempo_total, P, L, U
