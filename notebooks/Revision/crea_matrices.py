bddbe6ca0a3a4502d59a50492231f100959a03ab
alpika19186 <51685571+alpika19186@users.noreply.github.com>
2020-04-10 21:48:15 -0500

    Borrar crea_matrices.py, se agrego al script principal

--- notebooks/Revision/crea_matrices.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def crea_matrices(dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup):
-    n=np.random.randint(dim_limite_inf, dim_limite_sup)
-    A=np.array(np.random.randint(entradas_lim_inf,entradas_lim_sup, size=(n, n)))
-    return A,n
-
-    
