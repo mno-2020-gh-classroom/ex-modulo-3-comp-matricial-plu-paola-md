2e16565898839f9d5d96c239e72b35eafca56543
Karla Alfaro Pizaña <alpika_191@hotmail.com>
2020-04-16 00:38:15 -0500

    Agregar documentacion revision_bloques y borrar revision_bloques_2

--- notebooks/Revision/revision_modular.py
+++ notebooks/Revision/revision_modular.py
@@ -231,16 +231,16 @@ def revision_PLU(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,ent
 def revision_x(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup):
     '''
     El objetivo principal de esta función es verificar que la solución estimada del sistema de ecuaciones 
-    x_est sea la correcta al comparar con la x_real y generar un data frame con información sobre la 
-    dimensión de la matriz A, el tiempo de ejecución del algoritmo solve que resuelve el sistema, el 
-    estado de la verificación de la solución (Ej: Correcto o Incorrecto) y el error relativo entre x_est y 
-    x_real. 
+    x_est sea la correcta al compararla con la x_real y generar un data frame con información sobre la 
+    dimensión y condición de la matriz A, el tiempo de ejecución del algoritmo solve que resuelve el 
+    sistema, el estado de la verificación de la solución (Ej: Correcto o Incorrecto) y el error relativo 
+    entre x_est y x_real. 
     
-    Para cumplir con el objetivo principal, la función llama a otras funciones auxiliares: crea_matrices y 
-    solve_A_b, que se describen arriba.  Se le debe especificar el número de veces que se 
-    requiere quer se corra el algoritmo, el límite inferior y superior de la dimensión de la matriz y el 
-    límite inferior y superior de los números de entrada a la matriz. La función devuelve un data frame 
-    denominado resultados_x. 
+    Para cumplir con el objetivo principal, la función llama a otras funciones auxiliares: 
+    crea_matrices,condicion y  solve_A_b, que se describen arriba.  Se le debe especificar el número de 
+    veces que se requiere quer se corra el algoritmo, el límite inferior y superior de la dimensión de la 
+    matriz y el límite inferior y superior de los números de entrada a la matriz. La función devuelve un 
+    data frame denominado resultados_x. 
 
     ==========
     * Entradas:
@@ -252,7 +252,8 @@ def revision_x(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entra
         - entradas_lim_sup (integer): número entero que corresponde al número más grande que puede tener
         la matriz como entrada
     * Salidas:
-        - resultados_x (data frame): data frame con 4 columnas: dimension_A, condicion_A, tiempo_x, estado_x y error_x
+        - resultados_x (data frame): data frame con 5 columnas: dimension_A, condicion_A, tiempo_x, 
+        estado_x y error_absoluto
     ==========
     Ejemplo:
         >> num_corridas = 3
@@ -262,17 +263,17 @@ def revision_x(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entra
         >> entradas_lim_sup = 99 
         >> revision_x(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup)
         > resultados_x
-        >|          | dimension_A | condicion_A |  tiempo_x | estado_x |    error_x   |
-         |:--------:|:-----------:|:-----------:|:---------:|----------|--------------|
-         |     0    |      72     |  208.211964 |  0.212372 | Correcto | 1.579088e-14 |
-         |     1    |      56     | 1247.565615 |  0.102223 | Correcto | 2.442450e-13 |
-         |     2    |      32     | 2713.173540 |  0.025651 | Correcto | 5.905139e-13 |
+        >|          | dimension_A | condicion_A |  tiempo_x | estado_x | error_absoluto |
+         |:--------:|:-----------:|:-----------:|:---------:|----------|----------------|
+         |     0    |      72     |  208.211964 |  0.212372 | Correcto |  1.579088e-14  |
+         |     1    |      56     | 1247.565615 |  0.102223 | Correcto |  2.442450e-13  |
+         |     2    |      32     | 2713.173540 |  0.025651 | Correcto |  5.905139e-13  |
     '''
     dimension_A=[]
     condicion_A=[]
     estado_x=[]
     tiempo_x=[]
-    error_x=[]
+    error_absoluto=[]
     for i in range(0,num_corridas):
         
         #modulo que crea matrices de forma aleatoria
@@ -291,30 +292,57 @@ def revision_x(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entra
         status_x='Correcto' if np.allclose(x_est,x_real)==True else 'Incorrecto'
         estado_x.append(status_x)
         error_rel = np.mean(np.abs(x_real-x_est)/np.abs(x_real))
-        error_x.append(error_rel)
+        error_absoluto.append(error_rel)
          
-    data={'dimension_A':dimension_A,'condicion_A':condicion_A,'tiempo_x':tiempo_x,'estado_x':estado_x,'error_x':error_x}       
+    data={'dimension_A':dimension_A,'condicion_A':condicion_A,'tiempo_x':tiempo_x,'estado_x':estado_x,'error_absoluto':error_absoluto}       
     resultados_x=pd.DataFrame(data)
     return resultados_x
 
 def revision_bloques(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup):
-    """
-     Parametros
-    ----------   
-    num_corridas : numero de veces que queremos probar el algoritmo de "Solve_blocks"
-    dim_limite_inf : es el límite inferior de las entradas para la matriz random a generar.
-    dim_limite_sup : es el límite superior de las entradas para la matriz random a generar.
-    entradas_lim_inf : es el límite inferior del tamaño de la matriz, buscamos matrices de nxn.
-    entradas_lim_sup : es el límite superior del tamaño de la matriz, buscamos matrices de nxn.
-        
-    Salidas
-    -------
-    resultados : Un dataframe con el nombre de resultados, una vez evaluado el algoritmo con le número de corridas ingresadas.
-    """
+    '''
+    El objetivo principal de esta función es verificar que la solución estimada del sistema de ecuaciones 
+    solucion_bloques, del algoritmo por bloques, sea la correcta al compararla con la solucion_real y 
+    generar un data frame con información sobre la dimensión y condición de la matriz A, el tiempo de 
+    ejecución del algoritmo solve_blocks que resuelve el sistema, el estado de la verificación de la 
+    solución (Ej: Correcto o Incorrecto) y el error absoluto entre solucion_bloques y solucion_real. 
+    
+    Para cumplir con el objetivo principal, la función llama a otras funciones auxiliares: crea_matrices, 
+    condicion y resuelve_bloques, que se describen arriba.  Se le debe especificar el número de veces que 
+    se requiere quer se corra el algoritmo, el límite inferior y superior de la dimensión de la matriz y 
+    el límite inferior y superior de los números de entrada a la matriz. La función devuelve un data frame 
+    denominado resultados_bloques. 
+
+    ==========
+    * Entradas:
+        - num_corridas (integer): número de veces que se requiere quer se corra el algoritmo
+        - dim_limite_inf (integer): número entero que corresponde a la dimensión mínima de la matriz
+        - dim_liminte_sup (integer): número entero que corresponde a la dimensión máxima de la matriz
+        - entradas_lim_inf (integer): número entero que corresponde al número más chico que puede tener la 
+        matriz como entrada
+        - entradas_lim_sup (integer): número entero que corresponde al número más grande que puede tener
+        la matriz como entrada
+    * Salidas:
+        - resultados_bloques (data frame): data frame con 5 columnas: dimension_A, condicion_A, 
+        tiempo_bloques, solucion_bloques y error_absoluto
+    ==========
+    Ejemplo:
+        >> num_corridas = 3
+        >> dim_limite_inf = 2 
+        >> dim_liminte_sup = 10^4 
+        >> entradas_lim_inf = -99
+        >> entradas_lim_sup = 99 
+        >> revision_bloques(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup)
+        > resultados_bloques
+        >|          | dimension_A | condicion_A | tiempo_bloques | resultados_bloques |error_absoluto|
+         |:--------:|:-----------:|:-----------:|:--------------:|--------------------|--------------|
+         |     0    |      55     |  184.490589 |    0.088470    |       Correcto     | 4.676325e-09 |
+         |     1    |      18     |  62.869150  |    0.007024    |       Correcto     | 9.742454e-13 |
+         |     2    |       8     |  26.888839  |    0.002258    |       Correcto     | 4.218847e-14 |
+    '''
     dimension_A=[]
     condicion_A=[]
-    solucion_bloques=[]
     tiempo_bloques=[]
+    solucion_bloques=[]
     error_absoluto=[]
     
     for i in range(0,num_corridas):
@@ -345,59 +373,6 @@ def revision_bloques(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf
             solucion_bloques.append(status)
 
         
-    data={'dimension_A':dimension_A, 'condicion_A':condicion_A,'tiempo_bloques':tiempo_bloques,'solucion_bloques':solucion_bloques,'error_abs':error_absoluto}       
+    data={'dimension_A':dimension_A, 'condicion_A':condicion_A,'tiempo_bloques':tiempo_bloques,'solucion_bloques':solucion_bloques,'error_absoluto':error_absoluto}       
     resultados_bloques=pd.DataFrame(data)
     return resultados_bloques
-
-def revision_bloques_v2(num_corridas,dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup):
-    """
-     Parametros
-    ----------   
-    num_corridas : numero de veces que queremos probar el algoritmo de "Solve_blocks"
-    dim_limite_inf : es el límite inferior de las entradas para la matriz random a generar.
-    dim_limite_sup : es el límite superior de las entradas para la matriz random a generar.
-    entradas_lim_inf : es el límite inferior del tamaño de la matriz, buscamos matrices de nxn.
-    entradas_lim_sup : es el límite superior del tamaño de la matriz, buscamos matrices de nxn.
-        
-    Salidas
-    -------
-    resultados : Un dataframe con el nombre de resultados, una vez evaluado el algoritmo con le número de corridas ingresadas.
-    """
-    dimension_A=[]
-    condicion_A=[]
-    solucion_bloques=[]
-    tiempo_bloques=[]
-    error_absoluto=[]
-    
-    for i in range(0,num_corridas):
-        
-        #modulo que crea la matriz random
-        A,n=crea_matrices(dim_limite_inf,dim_limite_sup,entradas_lim_inf,entradas_lim_sup)
-        X=np.random.randint(entradas_lim_inf,entradas_lim_sup,size=(n))
-        B=A@X   
-        dimension_A.append(n)
-        
-        #modulo que calcula la condicion de A
-        cond=condicion(A)
-        condicion_A.append(cond)
-        
-        tiempo_total,X_algoritmo=resuelve_bloques(A,B)
-        tiempo_bloques.append(tiempo_total)
-        
-        error_abs=np.mean(np.fabs(X_algoritmo-X))
-        error_absoluto.append(error_abs)
-        
-        if(np.allclose(A@X_algoritmo,B)==True):
-            status='Correcto'
-            solucion_bloques.append(status)
-        else:
-            pprint.pprint('Incorrecto para A igual a:')
-            pprint.pprint(A)
-            status='Incorrecto'
-            solucion_bloques.append(status)
-
-        
-    data={'dimension_A':dimension_A, 'condicion_A':condicion_A,'tiempo_bloques':tiempo_bloques,'solucion_bloques':solucion_bloques,'error_abs':error_absoluto}       
-    resultados_bloques_v2=pd.DataFrame(data)
-    return resultados_bloques_v2
-
